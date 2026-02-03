72f100c9a581bc0e5c506cdfeea128995fe94261
kamcho <njokevin9@gmail.com>
2026-02-03 08:26:58 -0500

    participants

--- COD/urls.py
+++ COD/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from home.sitemaps import StaticViewSitemap, GameModeSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'gamemodes': GameModeSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('users/', include('users.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
