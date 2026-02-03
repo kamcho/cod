5044e099b32907da719f23211571cbd9b07b66d5
kamcho <njokevin9@gmail.com>
2026-02-03 08:47:28 -0500

    participants

--- COD/urls.py
+++ COD/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from home.sitemaps import StaticViewSitemap, GameModeSitemap
 
@@ -31,4 +32,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('users/', include('users.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
